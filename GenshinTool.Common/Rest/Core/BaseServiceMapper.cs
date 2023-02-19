5cb07c68e3c2bab6e971dce318bb5f27d8b92b7c
Thibault GOUBERT <goubertthibault@gmail.com>
2023-02-19 13:34:58 +0100

    SwaggerGen + core

---
+++ GenshinTool.Common/Rest/Core/BaseServiceMapper.cs
@@ -0,0 +1,11 @@
+﻿using Mapster;
+using MapsterMapper;
+
+namespace GenshinTool.Common.Rest.Core;
+
+public class BaseServiceMapper : ServiceMapper, IBaseMapper
+{
+    public BaseServiceMapper(IServiceProvider serviceProvider, TypeAdapterConfig config) : base(serviceProvider, config)
+    {
+    }
+}
