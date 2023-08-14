33cebc3acdf6ae32edd42c571133fb28e0361371
Thibault GOUBERT <goubertthibault@gmail.com>
2023-08-15 00:36:00 +0200

    we fix

--- GenshinTool-SPA/main.ts
+++ GenshinTool-SPA/main.ts
@@ -1,7 +1,8 @@
-const { app, BrowserWindow } = require('electron')
-const { autoUpdater } = require("electron-updater")
+import { app, BrowserWindow, screen } from 'electron';
+import { autoUpdater } from "electron-updater"; 
+import { windowPosition } from "config.json"
 
-let win;
+let win: BrowserWindow | any = null ;
 
 function createWindow() {
   win = new BrowserWindow({
@@ -10,10 +11,18 @@ function createWindow() {
     webPreferences: {
       nodeIntegration: true
     },
-    icon: "GenshinTool-SPA/src/assets/icons/icon.ico"
+    icon: "GenshinTool-SPA/src/assets/icons/icon.ico",
+    x: windowPosition.x,
+    y: windowPosition.y
   });
 
   win.loadURL(`file://${__dirname}/dist/genshin-tool/index.html`);
+
+  win.on('close', () => {
+    let values = (win as BrowserWindow).getPosition();
+    windowPosition.x = values[0];
+    windowPosition.y = values[1];
+  });
 
   win.on('closed', () => {
     win = null;
@@ -23,7 +32,7 @@ function createWindow() {
 app.on('ready', createWindow);
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+  if (process.platform !== 'darwin') {    
     app.quit();
   }
 });
